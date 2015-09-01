2739824eca25af49ecb978e219c00ea0ba68e5e7
Safiyat Reza <reza.safiyat@gmail.com>
2015-09-01 17:41:49 +0530

    Made a change in the autocomplete settings. Permanent for sure. ----> ; (define-key ac-complete-mode-map \t 'ac-expand)

--- loadpaths/epy/epy-completion.el
+++ loadpaths/epy/epy-completion.el
@@ -23,7 +23,7 @@
 (ac-config-default)
 
 ;; custom keybindings to use tab, enter and up and down arrows
-(define-key ac-complete-mode-map "\t" 'ac-expand)
+; (define-key ac-complete-mode-map "\t" 'ac-expand)
 (define-key ac-complete-mode-map "\r" 'ac-complete)
 (define-key ac-complete-mode-map "\M-n" 'ac-next)
 (define-key ac-complete-mode-map "\M-p" 'ac-previous)
