8287665ebed5ac2c79ac25ef0c0f8b537e1fdf47
Safiyat Reza <reza.safiyat@gmail.com>
2015-09-07 11:27:23 +0530

    Created keybindings.el. Editing custom.el.

---
+++ keybindings.el
@@ -0,0 +1,9 @@
+(global-set-key (kbd "C-/") 'comment-or-uncomment-region)
+
+
+(global-set-key [C-tab] 'next-multiframe-window)
+(global-set-key [C-S-iso-lefttab] 'previous-multiframe-window)
+
+(global-set-key [s-tab] 'next-buffer)
+(global-set-key [s-S-iso-lefttab] 'previous-buffer)
+
