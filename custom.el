b9912564c79d10cc9df033ee174b92ca7ef73b52
Safiyat Reza <reza.safiyat@gmail.com>
2015-09-08 17:45:20 +0530

    Changing the comment/uncomment function. Adding the ability to keep the commented/uncommented text selected. (set-mark-command) creating issues. Probably needs a param.

--- custom.el
+++ custom.el
@@ -1,28 +1,28 @@
-;TO DO
-;      Figure out custom-set-variables, setq and setq-default.
-;      Test various settings of fringe-mode.
-;      Multiple cursors.
-;      Virtualenv
-;      Python
-;      Flymake
-;      Pymacs
-;    Auto-complete
-;      ac-fuzzy-complete
-;      ac-user-dictionary   - ac-dictionary-directories
-;
-;      Override C-h to use keybindings.el
-;      Override C-w to use keybindings.el
-;      Override C-u to use keybindings.el
+;; TO DO
+;;      Figure out custom-set-variables, setq and setq-default.
+;;      Test various settings of fringe-mode.
+;;      Multiple cursors.
+;;      Virtualenv
+;;      Python
+;;      Flymake
+;;      Pymacs
+;;    Auto-complete
+;;      ac-fuzzy-complete
+;;      ac-user-dictionary   - ac-dictionary-directories
+
+;;      Override C-h to use keybindings.el
+;;      Override C-w to use keybindings.el
+;;      Override C-u to use keybindings.el
 
 
 (setq user-full-name "Md Safiyat Reza")
 (setq user-mail-address "md.reza@snapdeal.com")
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-;                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                        ;; ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))
 
-(defconst cust-emacs-dir     ; Find the path of the current directory.
+(defconst cust-emacs-dir     ;; Find the path of the current directory.
   (file-name-directory (or load-file-name
                            (when (boundp 'bytecomp-filename) bytecomp-filename)
                            buffer-file-name))
